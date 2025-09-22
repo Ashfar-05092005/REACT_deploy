eff5b904aa51fa9399ce2c2dd85d087f681ceae3
Ashfar-05092005 <mohammedashfar05092005@gmail.com>
2025-09-22 11:28:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>hi, this is m.mohammed ashfar</h1>
+     <h1 style={{backgroundColor: 'lightblue'}}>hi, this is m.mohammed ashfar. i am a full stack developer.
+      
+     </h1>
     </div>
   );
 }
