97b36b1d2467d57e9e54b8a0d09a495701d760db
MOHAMMED ASHFAR <mohammedashfar05092005@gmail.com>
2025-09-22 12:02:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App" style={{backgroundColor: '#4b3333', width: '100%', height: '100vh'}}>
-     <h1 style={{backgroundColor: 'lightblue'}}>hi, this is m.mohammed ashfar. i am a full stack developer.
+     <h1 style={{backgroundColor: 'lightblue'}}>this is m.mohammed ashfar. i am a full stack developer.
 
      </h1>
     </div>
